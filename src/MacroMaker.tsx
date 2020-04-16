ab0bd0c7e5c98446b03ae0f42981c7add9e23eb6
Francis <francis.screene@protonmail.com>
2020-04-16 22:03:57 +0100

    Ugh lex

--- src/MacroMaker.tsx
+++ src/MacroMaker.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { Modifiers } from "./modifiers";
-import { Pre, EditableText } from "@blueprintjs/core";
+import { Pre, EditableText, Switch } from "@blueprintjs/core";
+import dark from "./MacroMaker.module.css";
 
 interface IState {
     modifiers: string[];
     skillName: string;
+    darkMode: boolean;
 }
 
 export class MacroMaker extends React.Component<{}, IState> {
     public state = {
         modifiers: [],
-        skillName: ""
+        skillName: "",
+        darkMode: false
     };
 
     public render() {
+        // document.body.className = this.state.darkMode ? "bp3-dark" : "bp3-body";
+        const className = this.state.darkMode
+            ? "bp3-dark " + dark.dark + " " + dark.full
+            : "bp3-body";
         return (
-            <div>
+            <div className={className}>
                 <EditableText
                     placeholder="Enter spell name"
                     value={this.state.skillName}
@@ -27,9 +34,19 @@ export class MacroMaker extends React.Component<{}, IState> {
                     }}
                 />
                 <Pre>{this.getFormatted()}</Pre>
+
+                <Switch
+                    checked={this.state.darkMode}
+                    label="Dark Mode"
+                    onChange={this.toggleDarkMode}
+                />
             </div>
         );
     }
+
+    private toggleDarkMode = () => {
+        this.setState({ darkMode: !this.state.darkMode });
+    };
 
     private getFormatted = () => {
         let value = "#showtooltip " + this.state.skillName + "\n/cast";
